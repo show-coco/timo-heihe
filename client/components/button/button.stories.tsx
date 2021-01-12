eca925cfaa25a97df7b16cddc2cf0b7ab98dd7cd
Sho Sakai <show75@ezweb.ne.jp>
2021-01-12 20:30:54 +0900

    chore: setup Storybook

---
+++ client/components/button/button.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Meta } from "@storybook/react";
+import Button from "./button";
+
+export default {
+  title: 'Button',
+  component: Button,
+} as Meta;
+
+export const Basic = () => {
+  return <Button />;
+}
+
+
