e8f135cf381ab5120dd9cf382d5749c64e637cbd
Sho Sakai <show75@ezweb.ne.jp>
2021-01-12 21:16:16 +0900

    fix: _app.tsx and missing install @typescript-eslint/parser

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import React from "react";
-import "./styles.css";
+import "../styles/global.css";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
