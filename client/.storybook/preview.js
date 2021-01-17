42d32c13d255ca3a80d7537935866381736bb073
Sho Sakai <show75@ezweb.ne.jp>
2021-01-17 21:38:56 +0900

    chore: Enabling images in Storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,5 +1,13 @@
 import "../styles/globals.css"
+import * as nextImage from 'next/image';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => {
+    return <img {...props} />;
+  },
+});
