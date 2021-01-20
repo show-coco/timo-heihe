5f9f8be7b560ddc845b537c6826c147faede9691
Sho Sakai <show75@ezweb.ne.jp>
2021-01-20 23:26:42 +0900

    feat: Create heading component

--- client/components/heading/heading.tsx
+++ client/components/heading/heading.tsx
@@ -1,30 +1,51 @@
 import React from "react";
 
 type HeadingProps = {
-  children: React.ReactChild;
-  as?: "h1" | "h2" | "h3";
+  children: string;
+  as?: "h1Small" | "h1Big" | "h2" | "h3";
   className?: string;
 };
 
+const color = "text-gray-700";
+
 const styles = {
-  h1: "text-4xl",
-  h2: "text-3xl",
-  h3: "text-2lg",
+  h1Big: `text-4xl ${color}`,
+  h1Small: `text-lg font-bold ${color}`,
+  h2: `text-lg font-bold ${color}`,
+  h3: `text-base ${color}`,
 };
 
 export const Heading: React.FC<HeadingProps> = ({
   children,
-  as = "h1",
+  as = "h1Big",
   className,
 }: HeadingProps) => {
   const style = styles[as] + " " + className;
 
   switch (as) {
-    case "h1":
-      return <h1 className={style}>{children}</h1>;
+    case "h1Big":
+      return (
+        <h1 className={style} id={children}>
+          {children}
+        </h1>
+      );
+    case "h1Small":
+      return (
+        <h1 className={style} id={children}>
+          {children}
+        </h1>
+      );
     case "h2":
-      return <h2 className={style}>{children}</h2>;
+      return (
+        <h2 className={style} id={children}>
+          {children}
+        </h2>
+      );
     case "h3":
-      return <h3 className={style}>{children}</h3>;
+      return (
+        <h3 className={style} id={children}>
+          {children}
+        </h3>
+      );
   }
 };
