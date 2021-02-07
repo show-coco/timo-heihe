5a6d3383b3e07d108eb02c37cb3518903b84129d
Sho Sakai <show75@ezweb.ne.jp>
2021-02-07 19:04:15 +0900

    ui: Update layouts

--- client/pages/chat.tsx
+++ client/pages/chat.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Heading } from "../components/heading/heading";
 import { Template } from "../components/template/template";
+import { TextInput } from "../components/text-input/text-input";
 import { useAuthContext } from "../providers/useAuthContext";
 
 export default function ChatPage() {
@@ -10,9 +12,33 @@ export default function ChatPage() {
   return (
     <Template>
       <div className="grid grid-cols-chat h-full border-gray-200 border bg-white">
-        <div className="grid border-gray-200 border-r"></div>
-        <div className="grid border-gray-200 border-r"></div>
-        <div className="grid"></div>
+        <div className="flex items-center flex-col border-gray-200 border-r space-y-5 py-5">
+          <div className="bg-orange-500 w-12 h-12 rounded-md" />
+          <div className="bg-orange-500 w-12 h-12 rounded-md" />
+        </div>
+
+        <div className="flex flex-col border-gray-200 border-r">
+          <div className="flex items-center justify-center h-16 border-gray-200 border-b">
+            <Heading as="h1Small">Hirosaa</Heading>
+          </div>
+          <div className="flex-1"></div>
+        </div>
+
+        <div className="flex flex-col">
+          <div className="flex items-center border-gray-200 border-b h-16">
+            <Heading as="h3" className="ml-5">
+              #general
+            </Heading>
+          </div>
+
+          <div className="flex-1"></div>
+
+          <div className="h-16 items-center">
+            <div className="px-10">
+              <TextInput className="w-full p-3" />
+            </div>
+          </div>
+        </div>
       </div>
     </Template>
   );
