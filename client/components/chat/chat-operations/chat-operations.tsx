9702d35edec1bf485871ac57d61feb1eb91d3572
Sho Sakai <show75@ezweb.ne.jp>
2021-02-11 16:27:21 +0900

    ui: Create tooltip component

--- client/components/chat/chat-operations/chat-operations.tsx
+++ client/components/chat/chat-operations/chat-operations.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactComponent as EditPen } from "../../../assets/icons/plane-pen.svg";
+import { Tooltip } from "../../tooltip/tooltip";
 
 type Props = {
   className?: string;
@@ -10,9 +11,11 @@ export const ChatOperations: React.FC<Props> = ({ className }: Props) => {
     <div
       className={`rounded-md p-1 w-min chat-operations border flex space-x-4 ${className}`}
     >
-      <button className="h-6 w-6 p-1">
+      <button className="tool h-6 w-6 p-1">
         <EditPen />
       </button>
+      {/* FIXME: なぜかツールチップが表示されない 参考: https://zenn.dev/catnose99/articles/26bd8dac9ea5268486c8 */}
+      <Tooltip>編集する</Tooltip>
     </div>
   );
 };
