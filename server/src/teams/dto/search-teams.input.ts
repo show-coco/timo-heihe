5c45b9400bf63001a398ef40b7c867bcd9cc9a90
Sho Sakai <show75@ezweb.ne.jp>
2021-02-12 23:54:00 +0900

    feat: Implement search team by number of recruitment and team name and skills and categories

---
+++ server/src/teams/dto/search-teams.input.ts
@@ -0,0 +1,16 @@
+import { Field, InputType, Int } from '@nestjs/graphql';
+
+@InputType()
+export class SearchTeamInput {
+  @Field({ nullable: true })
+  name?: string;
+
+  @Field(() => [Int], { nullable: true })
+  skillIds?: number[];
+
+  @Field(() => [Int], { nullable: true })
+  categoryIds?: number[];
+
+  @Field(() => Int, { nullable: true })
+  recruitNumbers: number;
+}
