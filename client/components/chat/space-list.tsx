d561fcefec68b4b7f48167265863a58cb35a8219
Sho Sakai <show75@ezweb.ne.jp>
2021-02-14 15:57:49 +0900

    ui: Create button to create space

--- client/components/chat/space-list.tsx
+++ client/components/chat/space-list.tsx
@@ -12,17 +12,24 @@ export const SpaceList: React.FC<Props> = ({
   setSelectedSpace,
 }: Props) => {
   return (
-    <div className="flex items-center flex-col border-gray-200 border-r space-y-5 py-5">
-      {teams?.map((team) => (
-        <Avatar
-          src={team.icon || ""}
-          name={team.title}
-          key={team.id}
-          variant="square"
-          onClick={() => setSelectedSpace(team.id || 0)}
-          className="cursor-pointer"
-        />
-      ))}
-    </div>
+    <>
+      <div className="flex items-center flex-col border-gray-200 border-r space-y-3 py-5">
+        {teams?.map((team) => (
+          <button key={team.id}>
+            <Avatar
+              src={team.icon || ""}
+              name={team.title}
+              variant="square"
+              onClick={() => setSelectedSpace(team.id || 0)}
+              className="cursor-pointer"
+            />
+          </button>
+        ))}
+
+        <button className="h-12 w-12 rounded-md bg-gray-300">
+          <span className="text-white">＋</span>
+        </button>
+      </div>
+    </>
   );
 };
