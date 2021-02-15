bc26d8241a6a9f69602682e32771419fb68e86af
yoko <s1A121700885@toyo.jp>
2021-02-15 09:30:46 +0900

    feat:検索結果の値の取得確認

--- client/components/search-area/search-area.tsx
+++ client/components/search-area/search-area.tsx
@@ -7,18 +7,23 @@ import { NumberInput } from "../number-input/number-input";
 import {
   useFetchCategoryQuery,
   useFetchSkillQuery,
+  useSearchTeamQuery,
 } from "../../generated/types";
 import { useSearch } from "../../hooks/useSeach";
 type SearchAreaPros = {};
 export const SearchArea = () => {
   const { data: dataR } = useFetchCategoryQuery();
-
   const { data } = useFetchSkillQuery();
-  const [RecruitNumber, setRecruitNumber] = useState(0);
+  const [recruitNumbers, setRecruitNumbers] = useState(0);
   const [skills, setSkills] = useState<number>();
-  const [teamName, setTeamName] = useState<string>("");
+  const [name, setName] = useState<string>("");
   const [checkedCategories, setCheckedCategories] = useState({});
   const [checkedSkills, setCheckedSkills] = useState({});
+  const { data: dataS } = useSearchTeamQuery({
+    variables: {
+      input: { recruitNumbers, name },
+    },
+  });
 
   const handleChangeCategories = (e: React.FormEvent<HTMLInputElement>) => {
     setCheckedCategories({
@@ -37,18 +42,18 @@ export const SearchArea = () => {
   // デバック
   console.log("checkedItems: ", checkedCategories);
   console.log("checkedItems(スキル): ", checkedSkills);
-  console.log("チームネーム: ", teamName);
-  console.log("所属人数: ", RecruitNumber);
-
+  console.log("チームネーム: ", name);
+  console.log("所属人数: ", recruitNumbers);
+  console.log("検索結果: ", dataS);
   return (
     <div className="mt-5 pl-10">
       {/* テキストインプットと背景青 */}
       <div className="bg-blue-550 text-center rounded-t-md">
         <TextInput
-          onChange={(e) => setTeamName(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
           className="w-9/12 my-6"
           placeholder="チーム名で検索する"
-          value={teamName}
+          value={name}
         />
       </div>
       <div className="bg-white px-8">
@@ -57,7 +62,7 @@ export const SearchArea = () => {
           所属人数で絞る
         </Heading>
         <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
-          <NumberInput value={RecruitNumber} setValue={setRecruitNumber} />
+          <NumberInput value={recruitNumbers} setValue={setRecruitNumbers} />
         </div>
         {/* 技術で絞る */}
         <Heading as="h2" className="py-4">
