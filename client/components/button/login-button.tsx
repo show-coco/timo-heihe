1ee03e3cfea52baf052d9ed445c3352f3ffc9b88
yoko <s1A121700885@toyo.jp>
2021-02-19 18:07:02 +0900

    style:buttonに影を適用

--- client/components/button/login-button.tsx
+++ client/components/button/login-button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const LoginButton = () => {
   return (
-    <button className="py-1 px-3 align-middle ml-20 my-12 bg-gradient-to-r from-orange-400 to-orange-350 rounded-2xl text-white">
+    <button className="shadow-md py-1 px-3 align-middle ml-20 my-12 bg-gradient-to-r from-orange-400 to-orange-350 rounded-md text-white">
       <span className="text-white align-middle pr-1 font-bold text-2xl">G</span>
       Googleでログイン
     </button>
