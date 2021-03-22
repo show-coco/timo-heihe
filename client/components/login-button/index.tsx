a3ceac99089f5a32463ce46f32f5e41dc59d398c
yoko <s1A121700885@toyo.jp>
2021-03-22 22:29:34 +0900

    fix:code復元

--- client/components/login-button/index.tsx
+++ client/components/login-button/index.tsx
@@ -11,6 +11,9 @@ export const LoginButton: React.FC<Props> = ({ className }: Props) => {
       <button
         className={`px-3 py-1 text-white align-middle rounded-md shadow-sm bg-gradient-to-r from-orange-400 to-orange-350 hover:from-orange-600 hover:to-orange-350 ${className}`}
       >
+        <span className="pr-1 text-2xl font-bold text-white align-middle">
+          G
+        </span>
         Googleでログイン
       </button>
     </Link>
