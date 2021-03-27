f174e3e28decf6598a51a630c56df08984395891
Sho Sakai <show75@ezweb.ne.jp>
2021-03-27 18:47:24 +0900

    chore: Comment out unused code

--- client/components/chat/thread-list.tsx
+++ client/components/chat/thread-list.tsx
@@ -1,120 +1,120 @@
-import React, { useEffect } from "react";
-import {
-  ChatItemFragment,
-  ThreadListQuery,
-  useThreadListQuery,
-  useThreadSubscription,
-} from "../../generated/types";
-import { ChatItem } from "./chat-item/chat-item";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { useAuthContext } from "../../providers/useAuthContext";
+// import React, { useEffect } from "react";
+// import {
+//   ChatItemFragment,
+//   ThreadListQuery,
+//   useThreadListQuery,
+// useThreadSubscription,
+// } from "../../generated/types";
+// import { ChatItem } from "./chat-item/chat-item";
+// import InfiniteScroll from "react-infinite-scroll-component";
+// import { useAuthContext } from "../../providers/useAuthContext";
 
-type Props = {
-  channelId: number;
-  threads: ChatItemFragment[];
-  setThreads: React.Dispatch<React.SetStateAction<ChatItemFragment[]>>;
-};
+// type Props = {
+//   channelId: number;
+//   threads: ChatItemFragment[];
+//   setThreads: React.Dispatch<React.SetStateAction<ChatItemFragment[]>>;
+// };
 
-const currentDate = new Date().toISOString();
+// const currentDate = new Date().toISOString();
 
-export const ThreadList: React.FC<Props> = ({
-  channelId,
-  threads,
-  setThreads,
-}: Props) => {
-  const { id } = useAuthContext();
+// export const ThreadList: React.FC<Props> = ({
+//   channelId,
+//   threads,
+//   setThreads,
+// }: Props) => {
+//   const { id } = useAuthContext();
 
-  const { data, loading, error, fetchMore } = useThreadListQuery({
-    variables: {
-      input: {
-        channelId,
-        cursor: currentDate,
-      },
-    },
-  });
+//   const { data, loading, error, fetchMore } = useThreadListQuery({
+//     variables: {
+//       input: {
+//         channelId,
+//         cursor: currentDate,
+//       },
+//     },
+//   });
 
-  const { data: newThread } = useThreadSubscription({
-    variables: {
-      channelId,
-    },
-  });
+//   const { data: newThread } = useThreadSubscription({
+//     variables: {
+//       channelId,
+//     },
+//   });
 
-  useEffect(() => {
-    if (newThread) {
-      console.log("new threadssss", newThread);
+//   useEffect(() => {
+//     if (newThread) {
+//       console.log("new threadssss", newThread);
 
-      // threadサブスクリプション返却型からChatItemFragmentへ変換
-      const addedThread = newThread.threadAdded;
-      const thread: ChatItemFragment = {
-        id: addedThread.id,
-        createdAt: addedThread.createdAt,
-        channel: addedThread.channel,
-        text: addedThread.text,
-        user: addedThread.user,
-        numberOfMessages: addedThread.numberOfMessages,
-      };
+//       // threadサブスクリプション返却型からChatItemFragmentへ変換
+//       const addedThread = newThread.threadAdded;
+//       const thread: ChatItemFragment = {
+//         id: addedThread.id,
+//         createdAt: addedThread.createdAt,
+//         channel: addedThread.channel,
+//         text: addedThread.text,
+//         user: addedThread.user,
+//         numberOfMessages: addedThread.numberOfMessages,
+//       };
 
-      setThreads([thread, ...threads]);
-    }
-  }, [newThread, setThreads]);
+//       setThreads([thread, ...threads]);
+//     }
+//   }, [newThread, setThreads]);
 
-  useEffect(() => {
-    if (data?.threads) {
-      setThreads(data?.threads);
-      console.log("first threads", data.threads);
-    }
-  }, [data?.threads, setThreads]);
+//   useEffect(() => {
+//     if (data?.threads) {
+//       setThreads(data?.threads);
+//       console.log("first threads", data.threads);
+//     }
+//   }, [data?.threads, setThreads]);
 
-  if (channelId === 0)
-    return (
-      <div className="flex-1">
-        <p>ルームが選択されていません</p>
-      </div>
-    );
-  if (loading)
-    return (
-      <div className="flex-1">
-        <p>読み込み中</p>
-      </div>
-    );
-  if (error || !data)
-    return (
-      <div className="flex-1">
-        <p>メッセージの読み込みに失敗しました</p>
-      </div>
-    );
+//   if (channelId === 0)
+//     return (
+//       <div className="flex-1">
+//         <p>ルームが選択されていません</p>
+//       </div>
+//     );
+//   if (loading)
+//     return (
+//       <div className="flex-1">
+//         <p>読み込み中</p>
+//       </div>
+//     );
+//   if (error || !data)
+//     return (
+//       <div className="flex-1">
+//         <p>メッセージの読み込みに失敗しました</p>
+//       </div>
+//     );
 
-  return (
-    <div id="scrollDiv" className="h-70vh overflow-auto flex flex-col-reverse">
-      <InfiniteScroll
-        hasMore={true}
-        loader={<p>ロード中です</p>}
-        scrollableTarget="scrollDiv"
-        dataLength={threads.length}
-        style={{ display: "flex", flexDirection: "column-reverse" }}
-        inverse={true}
-        next={async () => {
-          const { data }: { data: ThreadListQuery } = await fetchMore({
-            variables: {
-              input: {
-                channelId,
-                cursor: threads[threads.length - 1].createdAt,
-              },
-            },
-          });
-          if (data.threads) {
-            setThreads([...threads, ...data.threads]);
-          }
-        }}
-      >
-        {threads.length === 0 ? (
-          <p>スレッドを送信してみましょう！</p>
-        ) : (
-          threads.map((thread, i) => (
-            <ChatItem item={thread} key={i} isMe={thread.user.id === id} />
-          ))
-        )}
-      </InfiniteScroll>
-    </div>
-  );
-};
+//   return (
+//     <div id="scrollDiv" className="flex flex-col-reverse overflow-auto h-70vh">
+//       <InfiniteScroll
+//         hasMore={true}
+//         loader={<p>ロード中です</p>}
+//         scrollableTarget="scrollDiv"
+//         dataLength={threads.length}
+//         style={{ display: "flex", flexDirection: "column-reverse" }}
+//         inverse={true}
+//         next={async () => {
+//           const { data }: { data: ThreadListQuery } = await fetchMore({
+//             variables: {
+//               input: {
+//                 channelId,
+//                 cursor: threads[threads.length - 1].createdAt,
+//               },
+//             },
+//           });
+//           if (data.threads) {
+//             setThreads([...threads, ...data.threads]);
+//           }
+//         }}
+//       >
+//         {threads.length === 0 ? (
+//           <p>スレッドを送信してみましょう！</p>
+//         ) : (
+//           threads.map((thread, i) => (
+//             <ChatItem item={thread} key={i} isMe={thread.user.id === id} />
+//           ))
+//         )}
+//       </InfiniteScroll>
+//     </div>
+//   );
+// };
--- client/hooks/useChat.ts
+++ client/hooks/useChat.ts
@@ -1,182 +1,182 @@
-import React from "react";
-import { useCallback, useEffect, useMemo, useState } from "react";
-import {
-  ChatItemFragment,
-  ChannelItemFragment,
-  RoomItemFragment,
-  useChatPageQuery,
-  useCreateThreadMutation,
-  useCreateChannelMutation,
-} from "../generated/types";
-import { useAuthContext } from "../providers/useAuthContext";
-import { useCreateSpace } from "./useCreateRoom.old";
-import { useModal } from "./useModal";
+// import React from "react";
+// import { useCallback, useEffect, useMemo, useState } from "react";
+// import {
+//   ChatItemFragment,
+//   ChannelItemFragment,
+//   RoomItemFragment,
+//   useChatPageQuery,
+//   useCreateThreadMutation,
+//   useCreateChannelMutation,
+// } from "../generated/types";
+// import { useAuthContext } from "../providers/useAuthContext";
+// import { useCreateSpace } from "./useCreateRoom.old";
+// import { useModal } from "./useModal";
 
-export const useChat = () => {
-  const { userId, id } = useAuthContext();
-  const [createThread] = useCreateThreadMutation();
-  const [selectedRoomId, setSelectedRoomId] = useState(0);
-  const [selectedChannelId, setSelectedChannelId] = useState(0);
-  const [rooms, setRooms] = useState<RoomItemFragment[]>([]);
-  const [threads, setThreads] = useState<ChatItemFragment[]>([]);
-  const [channels, setChannels] = useState<ChannelItemFragment[]>([]);
-  const [text, setText] = useState("");
-  const [channelName, setChannelName] = useState("");
+// export const useChat = () => {
+//   const { userId, id } = useAuthContext();
+//   const [createThread] = useCreateThreadMutation();
+//   const [selectedRoomId, setSelectedRoomId] = useState(0);
+//   const [selectedChannelId, setSelectedChannelId] = useState(0);
+//   const [rooms, setRooms] = useState<RoomItemFragment[]>([]);
+//   const [threads, setThreads] = useState<ChatItemFragment[]>([]);
+//   const [channels, setChannels] = useState<ChannelItemFragment[]>([]);
+//   const [text, setText] = useState("");
+//   const [channelName, setChannelName] = useState("");
 
-  const createSpace = useCreateSpace({ setRooms: setRooms, rooms: rooms });
+//   const createSpace = useCreateSpace({ setRooms: setRooms, rooms: rooms });
 
-  const createRoomModal = useModal();
+//   const createRoomModal = useModal();
 
-  const { data } = useChatPageQuery({
-    variables: {
-      userId,
-    },
-  });
+//   const { data } = useChatPageQuery({
+//     variables: {
+//       userId,
+//     },
+//   });
 
-  console.log("chat", data);
+//   console.log("chat", data);
 
-  const [createRoom] = useCreateChannelMutation();
+//   const [createRoom] = useCreateChannelMutation();
 
-  const selectedRoom = useMemo(() => {
-    return data?.user.rooms?.find((room) => room.id === selectedRoomId);
-  }, [data?.user.rooms, selectedRoomId]);
+//   const selectedRoom = useMemo(() => {
+//     return data?.user.rooms?.find((room) => room.id === selectedRoomId);
+//   }, [data?.user.rooms, selectedRoomId]);
 
-  const selectedChannel = useMemo(() => {
-    return channels?.find((room) => room.id === selectedChannelId);
-  }, [channels, selectedChannelId]);
+//   const selectedChannel = useMemo(() => {
+//     return channels?.find((room) => room.id === selectedChannelId);
+//   }, [channels, selectedChannelId]);
 
-  const onChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setText(event.target.value);
-  };
+//   const onChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setText(event.target.value);
+//   };
 
-  const onChangeRoomName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChannelName(event.target.value);
-  };
+//   const onChangeRoomName = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setChannelName(event.target.value);
+//   };
 
-  const onCreateRoom = async (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    try {
-      const { data } = await createRoom({
-        variables: {
-          input: {
-            name: channelName,
-            room: {
-              id: selectedRoomId,
-            },
-          },
-        },
-      });
+//   const onCreateRoom = async (
+//     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+//   ) => {
+//     try {
+//       const { data } = await createRoom({
+//         variables: {
+//           input: {
+//             name: channelName,
+//             room: {
+//               id: selectedRoomId,
+//             },
+//           },
+//         },
+//       });
 
-      // 作成したチャンネルを表示
-      if (data) {
-        const createdChannel: ChannelItemFragment = {
-          id: data.createChannel.id,
-          name: data.createChannel.name,
-        };
-        setChannels([...channels, createdChannel]);
-      }
-      createRoomModal.onClose();
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//       // 作成したチャンネルを表示
+//       if (data) {
+//         const createdChannel: ChannelItemFragment = {
+//           id: data.createChannel.id,
+//           name: data.createChannel.name,
+//         };
+//         setChannels([...channels, createdChannel]);
+//       }
+//       createRoomModal.onClose();
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-  const onClickSendButton = useCallback(
-    async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      console.log(id, text, selectedChannelId);
+//   const onClickSendButton = useCallback(
+//     async (event: React.FormEvent<HTMLFormElement>) => {
+//       event.preventDefault();
+//       console.log(id, text, selectedChannelId);
 
-      try {
-        const { data } = await createThread({
-          variables: {
-            input: {
-              channel: {
-                id: selectedChannelId,
-              },
-              user: {
-                id: id,
-              },
-              text: text,
-            },
-          },
-        });
-        if (data) {
-          const newThread = data.createThread;
-          const thread: ChatItemFragment = {
-            id: newThread.id,
-            createdAt: newThread.createdAt,
-            channel: newThread.channel,
-            text: newThread.text,
-            user: newThread.user,
-            numberOfMessages: newThread.numberOfMessages,
-          };
-          setThreads([thread, ...threads]);
-          setText("");
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    [id, text, selectedChannelId, createThread, threads]
-  );
+//       try {
+//         const { data } = await createThread({
+//           variables: {
+//             input: {
+//               channel: {
+//                 id: selectedChannelId,
+//               },
+//               user: {
+//                 id: id,
+//               },
+//               text: text,
+//             },
+//           },
+//         });
+//         if (data) {
+//           const newThread = data.createThread;
+//           const thread: ChatItemFragment = {
+//             id: newThread.id,
+//             createdAt: newThread.createdAt,
+//             channel: newThread.channel,
+//             text: newThread.text,
+//             user: newThread.user,
+//             numberOfMessages: newThread.numberOfMessages,
+//           };
+//           setThreads([thread, ...threads]);
+//           setText("");
+//         }
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     },
+//     [id, text, selectedChannelId, createThread, threads]
+//   );
 
-  useEffect(() => {
-    if (selectedRoom?.channels) {
-      setChannels(selectedRoom.channels);
-    }
-  }, [selectedRoom?.channels]);
+//   useEffect(() => {
+//     if (selectedRoom?.channels) {
+//       setChannels(selectedRoom.channels);
+//     }
+//   }, [selectedRoom?.channels]);
 
-  useEffect(() => {
-    if (data?.user.rooms) {
-      setRooms(data.user.rooms);
-    }
-  }, [data?.user.rooms]);
+//   useEffect(() => {
+//     if (data?.user.rooms) {
+//       setRooms(data.user.rooms);
+//     }
+//   }, [data?.user.rooms]);
 
-  // 最初に表示されるスペースIDをセット
-  useEffect(() => {
-    if (data?.user.rooms && data?.user.rooms.length > 0) {
-      const firstDisplayedTeamId = data?.user.rooms[0].id;
+//   // 最初に表示されるスペースIDをセット
+//   useEffect(() => {
+//     if (data?.user.rooms && data?.user.rooms.length > 0) {
+//       const firstDisplayedTeamId = data?.user.rooms[0].id;
 
-      if (firstDisplayedTeamId) {
-        setSelectedRoomId(firstDisplayedTeamId);
-      }
-    } else {
-      console.error("ルーム情報が正常に取得できませんでした");
-    }
-  }, [data?.user.rooms]);
+//       if (firstDisplayedTeamId) {
+//         setSelectedRoomId(firstDisplayedTeamId);
+//       }
+//     } else {
+//       console.error("ルーム情報が正常に取得できませんでした");
+//     }
+//   }, [data?.user.rooms]);
 
-  useEffect(() => {
-    if (channels && channels.length !== 0) {
-      setSelectedChannelId(channels[0].id);
-    } else {
-      setSelectedChannelId(0);
-    }
-  }, [channels, selectedRoomId]);
+//   useEffect(() => {
+//     if (channels && channels.length !== 0) {
+//       setSelectedChannelId(channels[0].id);
+//     } else {
+//       setSelectedChannelId(0);
+//     }
+//   }, [channels, selectedRoomId]);
 
-  return {
-    onClickSendButton,
-    onCreateRoom,
-    setter: {
-      setSelectedRoomId,
-      setSelectedChannelId,
-      onChangeText,
-      onChangeRoomName,
-      setThreads,
-    },
-    status: {
-      selectedRoomId,
-      selectedChannelId,
-      text,
-      channelName,
-      threads,
-      channels,
-      rooms,
-    },
-    selectedRoom,
-    selectedChannel,
-    data,
-    createRoomModal,
-    createSpace,
-  };
-};
+//   return {
+//     onClickSendButton,
+//     onCreateRoom,
+//     setter: {
+//       setSelectedRoomId,
+//       setSelectedChannelId,
+//       onChangeText,
+//       onChangeRoomName,
+//       setThreads,
+//     },
+//     status: {
+//       selectedRoomId,
+//       selectedChannelId,
+//       text,
+//       channelName,
+//       threads,
+//       channels,
+//       rooms,
+//     },
+//     selectedRoom,
+//     selectedChannel,
+//     data,
+//     createRoomModal,
+//     createSpace,
+//   };
+// };
