4921a9cd3adbabd4fc72b9c2020986c0e95fbc3c
Sho Sakai <show75@ezweb.ne.jp>
2021-03-30 23:19:13 +0900

    update: UI of MessageTimeline

--- client/components/message/message-timeline.tsx
+++ client/components/message/message-timeline.tsx
@@ -9,18 +9,20 @@ type Props = {
 
 export const MessageTimeline: React.FC<Props> = ({ users }: Props) => {
   return (
-    <div className="w-full border-r divide-y md:w-60">
+    <div className="w-full overflow-y-scroll border-r divide-y md:w-60">
       {users.map((user) => {
         return user ? (
           <div key={user?.id} className="flex items-center h-16 px-3">
-            <Avatar src={user?.avatar || ""} name={user?.name} />
+            <Avatar src={user?.avatar || ""} name={user?.name} size="small" />
 
             <div className="ml-3">
               <div className="flex justify-between">
                 <Ellipsis className="w-2/3 font-bold">veavenivaneivne</Ellipsis>
                 <span className="w-1/3 text-sm">7月21日</span>
               </div>
-              <Ellipsis className="w-32">最新のメッセージですaa</Ellipsis>
+              <Ellipsis className="w-32 text-sm">
+                最新のメッセージですaa
+              </Ellipsis>
             </div>
           </div>
         ) : (
