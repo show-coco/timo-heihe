365af7b4bb5fcb4f9c98c2c723215d8c33debe55
mi_iroha <chimosy21@gmail.com>
2021-03-31 23:02:11 +0900

    feat: モーダルに表示される各項目を押下して適切に画面遷移させる

--- client/components/navigation-modal/index.tsx
+++ client/components/navigation-modal/index.tsx
@@ -4,29 +4,32 @@ import { Avatar } from "../avatar/avatar";
 import { Heading } from "../heading/heading";
 import { LoginButton } from "../login-button";
 import { Modal, ModalProps } from "../modal/modal";
+import { useAuthContext } from "../../providers/useAuthContext";
 
 type Props = ModalProps & {
   isAuth: boolean;
 };
 
 export const NavigationModal: React.FC<Props> = (props: Props) => {
+  const { avatar, name, userId, logout } = useAuthContext();
+
   return (
     <Modal {...props}>
       {props.isAuth ? (
         <div className="flex flex-col">
           <div className="flex flex-row justify-center pb-3">
             <Avatar
-              src={
-                "http://flat-icon-design.com/f/f_object_96/s256_f_object_96_0bg.png"
-              }
-              name="chokin"
-              className="mr-3 align-middle cursor-pointer hover:opacity-80"
+              src={avatar}
+              name={name}
+              className="align-middle cursor-pointer hover:opacity-80"
             />
-            <span className="flex items-center">貯金 ブタ子</span>
+            <span className="flex items-center ml-3">{name}</span>
           </div>
           <div className="flex flex-col space-y-3 divide-y divide-gray-200">
             <span>
-              <Link href="/">プロフィール</Link>
+              <Link href="/user/[id]" as={`/user/${userId}`}>
+                プロフィール
+              </Link>
             </span>
             <span className="pt-3">
               <Link href="/">ルームを探す</Link>
@@ -35,10 +38,10 @@ export const NavigationModal: React.FC<Props> = (props: Props) => {
               <Link href="/">メッセージ</Link>
             </span>
             <span className="pt-3">
-              <Link href="/">受け取った申請</Link>
+              <Link href="/received-applications">受け取った申請</Link>
             </span>
             <span className="pt-3">
-              <Link href="/">ログアウト</Link>
+              <button onClick={logout}>ログアウト</button>
             </span>
           </div>
         </div>
