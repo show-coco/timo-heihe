f595815077876d2cf486cd3bf3fb914ef828615c
Sho Sakai <show75@ezweb.ne.jp>
2021-04-10 13:53:07 +0900

    feat: Update message timeline

--- client/components/message-timeline/item.tsx
+++ client/components/message-timeline/item.tsx
@@ -14,12 +14,13 @@ export const MessageTimelineItem: React.FC<Props> = ({ user }: Props) => {
       <div key={user.id} className="flex items-center h-16 px-3">
         <Avatar src={user.avatar || ""} name={user.name} size="small" />
 
-        <div className="ml-3">
+        <div className="w-full ml-3">
           <div className="flex justify-between">
-            <Ellipsis className="w-2/3 font-bold">veavenivaneivne</Ellipsis>
-            <span className="w-1/3 text-sm">7月21日</span>
+            <Ellipsis className="w-2/3 font-bold">{user.name}</Ellipsis>
+            {/* TODO */}
+            {/* <span className="w-1/3 text-sm">7月21日</span> */}
           </div>
-          <Ellipsis className="w-32 text-sm">最新のメッセージです</Ellipsis>
+          {/* <Ellipsis className="w-32 text-sm">最新のメッセージです</Ellipsis> */}
         </div>
       </div>
     </Link>
