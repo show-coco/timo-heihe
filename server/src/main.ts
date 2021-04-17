ee4f71b4048dee3bc44b8c4ae10a4fc550e9012b
Sho Sakai <show75@ezweb.ne.jp>
2021-04-17 15:38:30 +0900

    fix: deploy-backend.yml

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
     origin: process.env.FRONTEND_URL,
     credentials: true,
   });
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
