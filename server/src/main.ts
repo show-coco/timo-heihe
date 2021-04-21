a97bb1b33f68053671a6aafc92e14a2907b4090a
Sho Sakai <show75@ezweb.ne.jp>
2021-04-21 18:22:22 +0900

    faet: Add origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      process.env.FRONTEND_URL,
+      'https://timo-heihe-git-feature-deploy-show-coco.vercel.app',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT || 8080);
