df9fe41ae689c0d70c6b92991046bee55733aec9
Sho Sakai <show75@ezweb.ne.jp>
2021-04-24 13:45:15 +0900

    update scripts

--- server/scripts/config-compile.js
+++ server/scripts/config-compile.js
@@ -8,7 +8,7 @@ const template = fs.readFileSync('app.template.yaml').toString();
 let content = template;
 
 for (const match of template.matchAll(/\${{([A-Z_]*)}}/g)) {
-  const envName = `APP_${match[1]}`;
+  const envName = `${match[1]}`;
   content = content.replace(match[0], `'${process.env[envName]}'`);
 }
 
