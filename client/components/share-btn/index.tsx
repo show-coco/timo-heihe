865ef33129694966fffd4b08d1a0668e5750bbf6
Sho Sakai <show75@ezweb.ne.jp>
2021-05-01 16:33:47 +0900

    update: SNSシェアボタンにツールチップを表示

--- client/components/share-btn/index.tsx
+++ client/components/share-btn/index.tsx
@@ -5,6 +5,7 @@ import {
   FacebookIcon,
   TwitterIcon,
 } from "react-share";
+import ReactTooltip from "react-tooltip";
 
 type Props = {
   shareUrl: string;
@@ -12,11 +13,20 @@ type Props = {
 export const ShareBtn: VFC<Props> = ({ shareUrl }: Props) => {
   return (
     <div className="flex">
-      <FacebookShareButton url={shareUrl} title="#CloudCircle">
+      <ReactTooltip effect="solid" className="text-white bg-black-400" />
+      <FacebookShareButton
+        url={shareUrl}
+        title="#CloudCircle"
+        data-tip="Facebookへシェア"
+      >
         <FacebookIcon className="w-10 hover:opacity-80" round={true} />
       </FacebookShareButton>
 
-      <TwitterShareButton url={shareUrl} title="#CloudCircle">
+      <TwitterShareButton
+        url={shareUrl}
+        title="#CloudCircle"
+        data-tip="Twitterへシェア"
+      >
         <TwitterIcon className="w-10 ml-2 hover:opacity-70" round={true} />
       </TwitterShareButton>
     </div>
