a07d520fee5f27a2ff0824bfda5126bc7e75f93c
Sho Sakai <show75@ezweb.ne.jp>
2021-05-01 16:23:02 +0900

    feat: SNSシェアにハッシュタグを含めるよう変更

--- client/components/share-btn/index.tsx
+++ client/components/share-btn/index.tsx
@@ -11,12 +11,12 @@ type Props = {
 };
 export const ShareBtn: VFC<Props> = ({ shareUrl }: Props) => {
   return (
-    <div className="flex ">
-      <FacebookShareButton url={shareUrl}>
+    <div className="flex">
+      <FacebookShareButton url={shareUrl} title="#CloudCircle">
         <FacebookIcon className="w-10 hover:opacity-80" round={true} />
       </FacebookShareButton>
 
-      <TwitterShareButton url={shareUrl}>
+      <TwitterShareButton url={shareUrl} title="#CloudCircle">
         <TwitterIcon className="w-10 ml-2 hover:opacity-70" round={true} />
       </TwitterShareButton>
     </div>
